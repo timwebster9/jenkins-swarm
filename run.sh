b29697f271e786bf08113b238a3ecdb67db9bf8d
Tim Webster <tim.webster@gmail.com>
2017-05-14 02:01:48 +0100

    disable SSL

--- run.sh
+++ run.sh
@@ -13,4 +13,5 @@ if [ -n "${SWARM_EXECUTORS}" ]; then
     SWARM_ARGS="${SWARM_ARGS} -executors ${SWARM_EXECUTORS}"
 fi
 
-exec java -jar /usr/share/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar -fsroot /workspace ${SWARM_ARGS} -master http://jenkins-primary:${JENKINS_PORT:-8080}
+exec java -jar /usr/share/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar -fsroot /workspace ${SWARM_ARGS} \
+       -master http://jenkins-primary:${JENKINS_PORT:-8080} -disableSslVerification
